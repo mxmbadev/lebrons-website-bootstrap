94fa337e3a163a2c8963391c094e925dd2dbd4b0
Brandon Espinoza <espinozab100@gmail.com>
2020-08-11 12:56:50 -0500

    made active state work. Working on sticky sub nav

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
-    $('.navbar-nav .nav-item').click(function(){
-        $('.navbar-nav .nav-item').removeClass('active');
+    $('.nav .nav-item .nav-link').click(function(){
+        $('.nav .nav-item .nav-link').removeClass('active');
         $(this).addClass('active');
     })
+
+    $('.nav .nav-item .nav-link').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target
+          || $('[name=' + this.hash.slice(1) +']');
+          if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body')
+            .animate({scrollTop: targetOffset}, 1000);
+           return false;
+          }
+        }
+    });
+    // if ($(window).scrollTop() > 0) {
+    //     $(".nav").css("position", "fixed");
+    //     $(".nav").css("top", 0);
+    // } else {
+    //     $(".nav").css("position", "relative");
+    // }
 });
